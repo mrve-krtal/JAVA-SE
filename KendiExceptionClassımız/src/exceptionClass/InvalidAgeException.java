597b309366981beac854df9c4a12ccfcb9a74877
merve kartal <32229180+mrve-krtal@users.noreply.github.com>
2020-11-30 11:06:47 +0300

    java se

---
+++ KendiExceptionClassımız/src/exceptionClass/InvalidAgeException.java
@@ -0,0 +1,13 @@
+package exceptionClass;
+
+import java.io.IOException;
+
+public class InvalidAgeException extends IOException {
+
+	@Override
+	public void printStackTrace() {
+		
+		super.printStackTrace();
+	}
+
+}
---
+++ MiniProje-HarfFrekansı/src/harfFrekansı/Main.java
@@ -0,0 +1,41 @@
+package harfFrekansý;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TreeMap Kullanarak Bir Stringin içindeki harflerin frekansýný bulma
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Cümle giriniz: ");
+		String cumle=scanner.nextLine();
+		
+		Map<Character,Integer> frekans=new TreeMap<Character, Integer>();
+		
+		for (int i = 0; i < cumle.length(); i++) {
+			
+			char c=cumle.charAt(i);
+			
+			if (frekans.containsKey(c)) {
+				
+				frekans.replace(c, frekans.get(c)+1);
+				
+			}
+			else {
+				frekans.put(c,1);
+				
+			}
+			
+		}
+		
+		for (Map.Entry<Character, Integer> entry: frekans.entrySet()) {
+			System.out.println("Karakter : "+entry.getKey()+ " "+entry.getValue()+" tane var");
+			
+		}
+        
+	}
+
+}
